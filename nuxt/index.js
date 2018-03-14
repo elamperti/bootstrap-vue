85d4d4f983ebabba892a26cba349813bac37dc7b
Enrico Lamperti <elamperti@users.noreply.github.com>
2018-03-14 20:35:25 -0300

    Import individual components and directives

--- nuxt/index.js
+++ nuxt/index.js
@@ -14,16 +14,29 @@
 const { resolve } = require('path')
 
 module.exports = function nuxtBootstrapVue (moduleOptions) {
+  const defaultOptions = {
+    individualImports: false,
+    components: [],
+    directives: []
+  }
+
+  const options = Object.assign(defaultOptions, this.options.bootstrapVue, moduleOptions)
+
   // Conditionally require bootstrap original css too if not explicitly disabled
-  if (moduleOptions.css !== false) {
+  if (options.css !== false) {
     this.options.css.unshift('bootstrap/dist/css/bootstrap.css')
+  }
+
+  // Add library styles
+  if (options.bvCSS !== false) {
+    this.options.css.push('bootstrap-vue/dist/bootstrap-vue.css')
   }
 
   // Register plugin
   this.addPlugin({
     src: resolve(__dirname, 'plugin.js'),
     fileName: 'bootstrap-vue.js',
-    moduleOptions
+    options
   })
 }
 
--- nuxt/plugin.js
+++ nuxt/plugin.js
@@ -1,4 +1,15 @@
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
 
-Vue.use(BootstrapVue)
+<% if (!options.individualImports) { %>
+  import BootstrapVue from 'bootstrap-vue'
+  Vue.use(BootstrapVue)
+<% } else { %>
+  <% options.components.forEach(component => { %>
+        import <%= component.replace(/-/g, "") %>Component from 'bootstrap-vue/es/components/<%=component%>/<%=component%>'
+        Vue.component('b-<%=component%>', <%= component.replace(/-/g, "") %>Component)
+  <% }) %>
+  <% options.directives.forEach(directive => { %>
+    import <%=directive%>Directive from 'bootstrap-vue/es/directives/<%=directive%>/<%=directive%>'
+    Vue.component('b-<%=directive%>', <%=directive%>Directive)
+  <% }) %>
+<% } %>
